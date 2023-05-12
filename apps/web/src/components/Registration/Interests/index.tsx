faea40f3cf3e6c4e98e3810910778cdb0c186b00
0xdorifto <gastaosousafaria@tecnico.ulisboa.pt>
2023-05-12 23:22:05 +0100

    Add interests to flow

--- apps/web/src/components/Registration/Interests/index.tsx
+++ apps/web/src/components/Registration/Interests/index.tsx
@@ -1,13 +1,22 @@
 import Title from '@components/Shared/Title';
 import type { NextPage } from 'next';
-import { GridItemEight, GridLayout } from 'ui';
+import { Button, Card, GridItemEight, GridLayout } from 'ui';
+
+import ProfileInterests from '../../Settings/Interests/Interests';
 
 const Interests: NextPage = ({ nextStep }: any) => {
   return (
     <GridLayout>
-      <GridItemEight>
+      <GridItemEight className="m-auto flex max-w-4xl flex-col items-center gap-4">
         <Title>What are you interested in?</Title>
-        <button onClick={() => nextStep()}>next</button>
+
+        <Card className="p-5">
+          <ProfileInterests />
+        </Card>
+
+        <Button className="w-48" onClick={() => nextStep()}>
+          Next
+        </Button>
       </GridItemEight>
     </GridLayout>
   );
