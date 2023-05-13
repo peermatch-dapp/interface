0a2b23753d4bb4a7424613a1c79e1694055944fe
Yash Goyal <yashgo0018@gmail.com>
2023-05-13 14:21:12 +0100

    added chatbot api

---
+++ apps/web/src/pages/api/ai.ts
@@ -0,0 +1,10 @@
+import { updateState } from '@lib/openai';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  const { state, message } = req.body;
+  res.status(200).json(await updateState(state, message));
+}
